81e572ce241f008503b46d693f82142f04c2834e
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2019-03-03 14:53:51 -0500

    Made small incremental changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
+  mongoURI: "mongodb://simpleuser:Ytrewq123!@cluster0-shard-00-00-ttvgj.mongodb.net:27017,cluster0-shard-00-01-ttvgj.mongodb.net:27017,cluster0-shard-00-02-ttvgj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
   secretOrKey: "secret"
 };
