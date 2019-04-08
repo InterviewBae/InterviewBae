d55423f6f5a78e8c4a731f7c7fc278402f1f6667
Swapnil Tamrakar <swappy208@gmail.com>
2019-04-08 15:13:11 -0400

    Added Question Component

---
+++ client/src/components/dashboard/question.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getQuestion } from "../../actions/questionActions";
+
+var question = "";
+
+class QuestionBar extends Component {
+    componentWillMount() {
+        this.props.getQuestion();
+    }
+
+    componentDidUpdate()
+    {
+        question = this.props.question.data.Content;
+    }
+
+    render()
+    {
+        return(
+        <div style={{ width: "100vw" }}>
+        {question}
+        </div>);
+    }
+}
+
+const mapStateToProps = state => ({
+    question: state.currentQuestion
+});
+  
+export default connect(
+    mapStateToProps,
+    { getQuestion }
+  )(QuestionBar);
